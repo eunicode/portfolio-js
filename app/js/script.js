82069b764dc34c684925163fabccb8b1ea4524bb
Eunice Park <28845173+eunicode@users.noreply.github.com>
2019-02-05 23:48:48 -0800

    Rewrite class names using BEM methodology

--- app/js/script.js
+++ app/js/script.js
@@ -1,33 +1,47 @@
 // console.log(`Hi, JS is working`);
 
+// Group Projects
+
 const projectTitles_Group = ['Turtles Tab', 'Momentum Dash Clone', 'Pic Scavenger'];
 
 const projectImages_Group = ['turtles-tab', 'momentum-dash', 'pic-scavenger'];
+
+projectGithub_Group = [];
+
+projectDemos_Group = [];
+
+// Individual Projects
 
 const projectTitles_Indiv = ['ES6 Technical Documentation', 'Twitch Tracker', 'Survey Form'];
 
 const projectImages_Indiv = ['es6-docs', 'twitch-tracker', 'survey-form'];
 
+const projectGithub_Indiv = [];
+
+projectDemos_Indiv = [];
+
 function keepTrackOfH3() {
+  // Keep track of how many times `inner` fxn is called with closure
   let countCalls = 0;
 
-  const h3AnchorAll = document.querySelectorAll('.projects');
+  const h3AnchorAll = document.querySelectorAll('.projects__header');
 
   function inner(titles, imageLinks) {
+    // The first call attaches the generated section to first h3, and so forth
     const countIdx = countCalls;
     const h3Anchor = h3AnchorAll[countIdx];
 
     const container = document.createElement('div');
-    container.className = 'container';
+    container.className = 'projects__tile-container';
     h3Anchor.after(container);
 
     for (let i = 0; i < titles.length; i++) {
       const projectTile = document.createElement('div');
-      projectTile.className = 'project-tile';
+      projectTile.className = 'projects__tile';
       container.appendChild(projectTile);
 
       const projectImage = document.createElement('img');
-      projectImage.className = 'project-image';
+      projectImage.className = 'projects__tile-image';
       projectImage.src = `./images/${imageLinks[i]}.png`
       projectImage.alt = titles[i];
       projectTile.appendChild(projectImage);
@@ -66,6 +80,7 @@ Whenever there's a change in the files, we re-process, re-copy, and re-load.
 The files served in development and production will be the build folder.
 
 Get rid of global variables
+Package into object
 */
 
 // NOTES
@@ -74,4 +89,34 @@ CLOSURE
 https://github.com/eunicode/algos/blob/master/csx/closure-scope-excon/cycleIterator.js
 https://github.com/eunicode/algos/blob/master/csx/closure-scope-excon/censor.js
 
+*/
+
+// LESSONS LEARNED
+/*
+How TO - Fixed Menu
+https://www.w3schools.com/howto/howto_css_fixed_menu.asp
+
+How TO - Sticky/Affix Navbar
+https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+
+How TO - On Scroll Header
+https://www.w3schools.com/howto/howto_js_sticky_header.asp
+
+Treehouse CSS Grid - Practice Flexible Grid Layout
+
+BEM
+
+BEM Quick Start
+https://en.bem.info/methodology/quick-start/#should-i-create-a-block-or-an-element
+
+Wrong
+projects__tile__image
+
+Alternative #1
+projects__tile
+projects__tile-image
+
+Alternative #2 Create a new block
+projects-tile
+projects-tile__image
 */
